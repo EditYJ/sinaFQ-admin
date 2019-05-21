beece52cb42cfe1fdcae69a9ee4b83bc66fdb0a9
wangshi <wangshi@xiaoxiangyoupin.cn>
2019-05-21 11:28:12 +0800

    新增图片点击放大组件

---
+++ src/components/bigImg/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="pop-box" v-show="bigImgShow" @click="bigImgShow = !bigImgShow">
+            <div class="imgPopBox">
+                <img :src="bigImgSrc">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            bigImgShow : false,
+            bigImgSrc : '',
+        }
+    },
+    methods : {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .pop-box{
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 100000;
+        background-color: rgba(0,0,0,.6);
+    }
+    .imgPopBox{
+        width: 500px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        -webkit-transform: translate(-50%,-50%);
+    }
+    .imgPopBox img {
+        width: 500px;
+    }
+</style>
